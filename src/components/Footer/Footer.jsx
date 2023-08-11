4cd9c31223440430531141166474b16a793cd7ba
Caitlin Donahue <caitlindonahue@Caitlins-MacBook-Air.local>
2023-08-10 21:27:07 -0400

    added to components and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,13 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className = "footer">
-      <a href="https://github.com/CaitD35/" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a>
       <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      
+      <div className="footer-contact">
+        <p>Phone: 267-701-7204</p>
+        <p>Email: <a href="mailto:cdonahue8749@gmail.com">cdonahue8749@gmail.com</a></p>
+      </div>
     </footer>
   );
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,7 +6,9 @@ function Portfolio() {
     
     const projects = [
         { title: "Workout Tracker", imgSrc: "./images/workouttracker.png", deployedLink: "https://workout-plus-c7901a67a230.herokuapp.com", githubLink: "https://github.com/Lalu423/workout-tracker.git " },
-        // ... add other projects
+        { title: "Activity Roulette", imgSrc: "./images/AR_project.png", deployedLink: "https://sarroyo551.github.io/Activity-Roulette/", githubLink: "https://github.com/sarroyo551/Activity-Roulette.git" },
+        { title: "Horiseon Refactor", imgSrc: "./images/hr.png", deployedLink: "https://caitd35.github.io/horiseon-refactor-/", githubLink: "https://github.com/CaitD35/horiseon-refactor-.git" }, 
+        { title: "Pre-work Study Guide", imgSrc: "./images/pr.png", deployedLink: "https://caitd35.github.io/prework-study-guide/", githubLink: "https://github.com/CaitD35/prework-study-guide.git" },   
     ];
 
     return (
